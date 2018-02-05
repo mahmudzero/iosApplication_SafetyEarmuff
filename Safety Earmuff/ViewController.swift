591b135e35639fcb1930f91abd36994194e2cf73
Mahmud Ahmad <duelwith_mahmud@live.com>
2018-02-05 08:13:05 -0500

    Initial Commit

---
+++ Safety Earmuff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Safety Earmuff
+//
+//  Created by Mahmud Ahmad on 2/5/18.
+//  Copyright © 2018 Mahmud Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Safety EarmuffTests/Safety_EarmuffTests.swift
@@ -0,0 +1,36 @@
+//
+//  Safety_EarmuffTests.swift
+//  Safety EarmuffTests
+//
+//  Created by Mahmud Ahmad on 2/5/18.
+//  Copyright © 2018 Mahmud Ahmad. All rights reserved.
+//
+
+import XCTest
+@testable import Safety_Earmuff
+
+class Safety_EarmuffTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
